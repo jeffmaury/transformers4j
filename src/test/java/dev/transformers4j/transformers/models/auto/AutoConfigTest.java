bbfdca9cd9fed6056ee84cc47474edb082ac5e19
Jeff MAURY <jmaury@redhat.com>
2024-08-23 16:26:51 +0200

    fix: add new local config test
    Signed-off-by: Jeff MAURY <jmaury@redhat.com>

--- src/test/java/dev/transformers4j/transformers/models/auto/AutoConfigTest.java
+++ src/test/java/dev/transformers4j/transformers/models/auto/AutoConfigTest.java
@@ -1,15 +1,30 @@
 package dev.transformers4j.transformers.models.auto;
 
+import dev.transformers4j.transformers.models.bert.BertConfig;
+import dev.transformers4j.transformers.models.roberta.RobertaConfig;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
 import java.nio.file.Path;
 import java.util.Collections;
 
+import static dev.transformers4j.transformers.TestingUtils.get_tests_dir;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertInstanceOf;
+
 public class AutoConfigTest {
+    private static final Path SAMPLE_ROBERTA_CONFIG = get_tests_dir("fixtures/dummy-config.json");
+
     @Test
     public void test_config_from_model_shortcut() throws IOException {
         var config = AutoConfig.from_pretrained(Path.of("google-bert/bert-base-uncased"));
+        assertInstanceOf(BertConfig.class, config);
+    }
+
+    @Test
+    public void test_config_model_type_from_local_file() throws IOException {
+        var config = AutoConfig.from_pretrained(SAMPLE_ROBERTA_CONFIG);
+        assertInstanceOf(RobertaConfig.class, config);
     }
 
 }
